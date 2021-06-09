a7cf582343732063e2364a3098782cfa4f70aa5b
jessejohns-1 <84858554+jessejohns-1@users.noreply.github.com>
2021-06-09 16:51:43 -0400

    mvp

--- index.js
+++ index.js
@@ -45,10 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(flavors){
-    var copy = originalFlavors.slice();
-}    
+function copy(flavorscopy){
+    const cloneflavor = {...originalFlavors}
+    return flavorscopy;  
 
+} 
+ 
 
 
 
@@ -64,10 +66,11 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(originalFlavors){
+    console.log(is31Flavors.length)
+    if 
+    
 }
-
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
